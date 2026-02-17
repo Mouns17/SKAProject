b4d5eca9e0451d630e28fd4817857252053111a1
Mouns17 <nozerit777@gmail.com>
2026-02-18 01:05:05 +0300

    Test commit

--- SKAProject/LogginWindow.xaml.cs
+++ SKAProject/LogginWindow.xaml.cs
@@ -36,7 +36,7 @@ namespace SKAProject
 
         private void OpenRegister_Click(object sender, RoutedEventArgs e)
         {
-            RegisterWindow tungtungtungsahur = new RegisterWindon();
+            RegisterWindow tungtungtungsahur = new RegisterWindow();
             tungtungtungsahur.Show();
             this.Close();
         }
