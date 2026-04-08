ac91a9004d0024b3bdac3d3cde4b54747c6f5abb
Mouns17 <nozerit777@gmail.com>
2026-04-08 19:36:15 +0300

    Связал все с БД + А еще я ебать придумал хуйню чтоб не делать два окна регистрации

--- SKAProject/RegistrationWindow.xaml.cs
+++ SKAProject/RegistrationWindow.xaml.cs
@@ -29,42 +29,81 @@ namespace SKAProject
             InitializeComponent();
         }
 
-        /*
-        private void Next_Click(object sender, RoutedEventArgs e)
-        {
-            // Получем данные и удаляем пробелы в них
-            string login = LoginBox.Text.Trim();
-            string password = PassBox.Text.Trim();
-            string email = EmailBox.Text.Trim();
-            string phone = PhoneBox.Text.Trim();
-
-            RegisterNextWindow next = new RegisterNextWindow(login, password, email, phone);
-            next.Show();
-            this.Close();
-        }
-
-        private void BtnClose1_Click(object sender, RoutedEventArgs e)
-        {
-            Close();
-        }
-
-        private void BtnClose2_Click(object sender, RoutedEventArgs e)
-        {
-            this.WindowState = WindowState.Minimized;
-        }
-        */
-
         private void MainBorder_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
         {
             DragMove();
         }
 
-        private void RegisterUser(object sender, RoutedEventArgs e)
+        private void BtnNextReg(object sender, RoutedEventArgs e)
         {
+            TBoxLogin.Visibility = Visibility.Collapsed;
+            TBoxPassword.Visibility = Visibility.Collapsed;
+            TBoxRePassword.Visibility = Visibility.Collapsed;
+
+            TBoxFirstName.Visibility = Visibility.Visible;
+            TBoxLastName.Visibility = Visibility.Visible;
+            TBoxMiddleName.Visibility = Visibility.Visible;
+
+            BtnNextRegVis.Visibility = Visibility.Collapsed;
+            BtnRegisterVis.Visibility = Visibility.Visible;
+        }
+
+
+        private async void BtnRegister(object sender, RoutedEventArgs e)
+        {
+            string login = TBoxLogin.Text.Trim();
+            string password = TBoxPassword.Text.Trim();
+            string firstname = TBoxFirstName.Text.Trim();
+            string lastname = TBoxLastName.Text.Trim();
+            string middlename = TBoxMiddleName.Text.Trim();
+
+            try
+            {
+                // Подключение к бд
+                using (MySqlConnection conn = DataBase.GetConnection())
+                {
+                    await conn.OpenAsync();
+
+                    using (MySqlTransaction tx = conn.BeginTransaction())
+                    {
+                        MySqlCommand cmdUser = new MySqlCommand(@"INSERT INTO Users(Login, Password, FirstName, LastName, MiddleName) VALUES(@lg,@ps, @fn, @ln, @mn); SELECT LAST_INSERT_ID();", conn, tx);
+
+                        // Передаём параметры в запрос
+                        cmdUser.Parameters.AddWithValue("@lg", login);
+                        cmdUser.Parameters.AddWithValue("@ps", password);
+                        cmdUser.Parameters.AddWithValue("@fn", firstname);
+                        cmdUser.Parameters.AddWithValue("@ln", lastname);
+                        cmdUser.Parameters.AddWithValue("@mn", middlename);
+
+                        // Выполняем запрос
+                        await cmdUser.ExecuteScalarAsync();
+
+                        // Подтверждение транзакции
+                        tx.Commit();
+                    }
+                }
+
+                // Проверка на пустые поля
+                if (string.IsNullOrWhiteSpace(login) || string.IsNullOrWhiteSpace(password))
+                {
+                    MessageBox.Show("Заполните все поля");
+                    return;
+                }
+
+                MessageBox.Show("Регистрация завершена");
+                new LoginWindow().Show();
+                Close();
+            }
+
+            // При каких то ошибках связанных с БД
+            catch (Exception ex)
+            {
+                MessageBox.Show("Ошибка регистрации: " + ex.Message);
+            }
 
         }
 
-        private void BackToLogin(object sender, RoutedEventArgs e)
+        private void BtnBack(object sender, RoutedEventArgs e)
         {
             LoginWindow login = new LoginWindow();
             login.Show();
