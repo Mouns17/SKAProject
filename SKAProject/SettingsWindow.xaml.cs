8f80fcdd69fc73dbb5483aa14b35a83705356cd7
Mouns17 <nozerit777@gmail.com>
2026-04-17 02:39:02 +0300

    Changes Window's #001

--- SKAProject/SettingsWindow.xaml.cs
+++ SKAProject/SettingsWindow.xaml.cs
@@ -14,9 +14,6 @@ using System.Windows.Shapes;
 
 namespace SKAProject
 {
-    /// <summary>
-    /// Логика взаимодействия для SettingsWindow.xaml
-    /// </summary>
     public partial class SettingsWindow : Window
     {
         public SettingsWindow()
