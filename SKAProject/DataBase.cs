45b4120288641db449e0a31c59b32b31a0a2b503
Mouns17 <nozerit777@gmail.com>
2026-04-18 23:32:39 +0300

    Change Window's #7

--- SKAProject/DataBase.cs
+++ SKAProject/DataBase.cs
@@ -9,7 +9,7 @@ namespace SKAProject
 {  
     public static class DataBase
     {
-        private static string connStr = "server=localhostdatabase=SKAProjec;user=root;password=123456;SslMode=None";
+        private static string connStr = "server=localhost;database=SKADataBase;user=root;password=123456;SslMode=None";
 
         public static MySqlConnection GetConnection()
         {
