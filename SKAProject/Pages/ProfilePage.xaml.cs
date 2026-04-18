97a1299245f62bdeef264adb2173ba7d15dfd73b
Mouns17 <nozerit777@gmail.com>
2026-04-19 00:08:29 +0300

    Change ProfilePage #4

--- SKAProject/Pages/ProfilePage.xaml.cs
+++ SKAProject/Pages/ProfilePage.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection.Emit;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -22,6 +23,12 @@ namespace SKAProject.Pages
     {
         public ProfilePage()
         {
+            var timer = new System.Windows.Threading.DispatcherTimer();
+            timer.Interval = new TimeSpan(0, 0, 1);
+            timer.IsEnabled = true;
+            timer.Tick += (o, t) => { TBlockTime.Text =  DateTime.Now.ToString("HH mm"); };
+            timer.Start();
+
             InitializeComponent();
         }
     }
