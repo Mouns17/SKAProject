fb30d0f9715caa457a3fbb0491dfb3ee622c2836
Mouns17 <nozerit777@gmail.com>
2026-05-05 01:49:46 +0300

    Change Window's #29

--- SKAProject/Models/ProfileModel.cs
+++ SKAProject/Models/ProfileModel.cs
@@ -8,15 +8,15 @@ namespace SKAProject.Models
 {
     public class ProfileModel
     {
-            public string Login { get; set; }
-            public string FirstName { get; set; }
-            public string LastName { get; set; }
-            public string MiddleName { get; set; }
-            public string Email { get; set; }
-            public string Phone { get; set; }
-            public string Role { get; set; }
-            public string Post { get; set; }
-            public string Department { get; set; }
-            public string Organization { get; set; }
+        public string Login { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string MiddleName { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public string Role { get; set; }
+        public string Post { get; set; }
+        public string Department { get; set; }
+        public string Organization { get; set; }
     }
 }
